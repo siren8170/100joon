0f60c6319d77db218a9640d51864a54bd40f0151
siren8170 <kingofredwind@gmail.com>
2025-03-18 18:22:20 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a, s = 0;
+    scanf("%d", &a);
+    for(int i = 1; i <= a; i++)
+        s = s + i;
+    printf("%d", s);
+}
