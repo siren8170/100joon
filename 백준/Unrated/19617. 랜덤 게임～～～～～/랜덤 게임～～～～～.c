bc6e5be0646b48d12207327d382e09525481b5cf
siren8170 <kingofredwind@gmail.com>
2025-07-03 17:09:07 +0900

    [Unrated] Title: 랜덤 게임~~~~~, Time: 0 ms, Memory: 1116 KB, Score: 47 point -BaekjoonHub

--- 백준/Unrated/19617. 랜덤 게임～～～～～/랜덤 게임～～～～～.c
+++ 백준/Unrated/19617. 랜덤 게임～～～～～/랜덤 게임～～～～～.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-int main(){
-    printf("%d", 12345678);
+int main() {
+    srand(time(NULL));
+    printf("%d\n", rand());
+    return 0;
 }
